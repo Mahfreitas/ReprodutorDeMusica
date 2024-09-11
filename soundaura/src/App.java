1d3b3e24a2bd53726325927a6a562622f2663295
S0l3r4 <thomasdamasena2@gmail.com>
2024-09-11 17:41:12 -0300

    Iniciando Porjeto de Novo

--- soundaura/src/App.java
+++ soundaura/src/App.java
@@ -1,8 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Reprodutor de musicas com JavaFX");
-        System.out.println("Reprodutor de musicas com JavaFX");
-        System.out.println("Reprodutor de musicas com JavaFX");
         
     }
 }
