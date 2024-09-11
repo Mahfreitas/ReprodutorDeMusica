9780d92892ba7bba50520b4596d4ddf59ae82977
S0l3r4 <thomasdamasena2@gmail.com>
2024-09-11 17:30:44 -0300

    Linha duplicada para teste de commit - Thomas

--- soundaura/src/App.java
+++ soundaura/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Reprodutor de musicas com JavaFX");
+        System.out.println("Reprodutor de musicas com JavaFX");
     }
 }
