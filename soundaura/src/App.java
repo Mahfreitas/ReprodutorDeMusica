ff824ccf38a45fd02e20d56fbe65787925de196d
Mahfreitas <dudafreitaneves07@gmail.com>
2024-09-11 17:36:11 -0300

    adicionando a terceira linha

--- soundaura/src/App.java
+++ soundaura/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Reprodutor de musicas com JavaFX");
         System.out.println("Reprodutor de musicas com JavaFX");
+        System.out.println("Reprodutor de musicas com JavaFX");
+        
     }
 }
