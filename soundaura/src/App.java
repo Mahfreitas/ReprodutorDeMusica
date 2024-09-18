257f3778b9772e516a4d9ddbefd8036ac6f36f8f
Aracnocat <yoshiokoppa@gmail.com>
2024-09-18 16:55:29 -0300

    Recomecando - Mikael

--- soundaura/src/App.java
+++ soundaura/src/App.java
@@ -1,8 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello FX");
-        System.out.println("Bom dia Yasmin");
-        System.out.println("Bom dia Yasmin");
-        System.out.println("Bom dia Mikael");
     }
 }
