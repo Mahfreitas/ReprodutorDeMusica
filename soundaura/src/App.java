520b69aa62ea156ccbc9f4866bad8fed86ef8978
kodkall <mbeatrizfsoliveira@gmail.com>
2024-09-18 16:21:02 -0300

    Primeiro Commit - Kall

--- soundaura/src/App.java
+++ soundaura/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello FX");
         System.out.println("Bom dia Yasmin");
-        
+        System.out.println("Bom dia Yasmin");
+        System.out.println("Bom dia Yasmin");
     }
 }
