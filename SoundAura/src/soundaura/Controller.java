fb60a69e4cfd1470e199f865c3981839f10d7f2c
S0l3r4 <thomasdamasena2@gmail.com>
2024-10-16 01:05:46 -0300

    Primeira Tela - Thomas

---
+++ SoundAura/src/soundaura/Controller.java
@@ -0,0 +1,5 @@
+package soundaura;
+
+public class Controller {
+    
+}
--- SoundAura/src/soundaura/PaginaCadastro.java
+++ SoundAura/src/soundaura/PaginaCadastro.java
@@ -1,5 +1,38 @@
 package soundaura;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class PaginaCadastro {
-    
+
+    @FXML
+    private Button BotaoCadastro;
+
+    @FXML
+    private Hyperlink LinkLogin1;
+
+    @FXML
+    private Hyperlink LinkLogin2;
+
+    @FXML
+    private Pane PaneTopoTela;
+
+    @FXML
+    private TextField TextFielEmailCad;
+
+    @FXML
+    private TextField TextFieldConfSenhaCad;
+
+    @FXML
+    private TextField TextFieldSenhaCad;
+
+    @FXML
+    void RealizarCadastro(ActionEvent event) {
+
+    }
+
 }
