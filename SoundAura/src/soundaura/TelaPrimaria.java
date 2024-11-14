c9b89eb884d291044fae08a5101fbd8e2c192b8b
S0l3r4 <thomasdamasena2@gmail.com>
2024-11-13 21:16:18 -0300

    Arrumando GripPane Lateral - Todos

--- SoundAura/src/soundaura/TelaPrimaria.java
+++ SoundAura/src/soundaura/TelaPrimaria.java
@@ -1,11 +1,12 @@
 package soundaura;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class TelaPrimaria {
 
     @FXML
-    private Label emailUsuario;
+    void trocarImage(MouseEvent event) {
+
+    }
 
 }
