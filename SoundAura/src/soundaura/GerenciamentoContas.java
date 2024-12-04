80836cc9751d5df5bef1562d147076bc3164a5b1
S0l3r4 <thomasdamasena2@gmail.com>
2024-12-04 17:51:37 -0300

    Colocando Banco de Dados LocalHost

--- SoundAura/src/soundaura/GerenciamentoContas.java
+++ SoundAura/src/soundaura/GerenciamentoContas.java
@@ -1,5 +1,6 @@
 package soundaura;
 
+import java.io.File;
 import java.sql.*;
 import javafx.scene.control.Alert;
 
@@ -10,7 +11,7 @@ public class GerenciamentoContas {
         if (!email.isEmpty() && !senha.isEmpty() && !confSenha.isEmpty()) {
             if (senha.equals(confSenha)) {
                 try (Connection conn = DatabaseConnection.getConnection()) {
-                    String checkQuery = "SELECT * FROM Users WHERE Email = ?";
+                    String checkQuery = "SELECT * FROM usuario WHERE email_usuario = ?";
                     try (PreparedStatement checkStmt = conn.prepareStatement(checkQuery)) {
                         checkStmt.setString(1, email);
                         ResultSet resultSet = checkStmt.executeQuery();
@@ -21,7 +22,7 @@ public class GerenciamentoContas {
                             cadastroErro.setContentText("O usuário em questão já existe, tente novamente");
                             cadastroErro.showAndWait();
                         } else {
-                            String insertQuery = "INSERT INTO Users (Email, PasswordHash) VALUES (?, ?)";
+                            String insertQuery = "INSERT INTO usuario (Email, PasswordHash) VALUES (?, ?)";
                             try (PreparedStatement insertStmt = conn.prepareStatement(insertQuery)) {
                                 insertStmt.setString(1, email);
                                 insertStmt.setString(2, senha); 
@@ -33,6 +34,12 @@ public class GerenciamentoContas {
                                 cadastroRealizado.setContentText("O seu cadastro foi realizado com sucesso");
                                 cadastroRealizado.showAndWait();
 
+                                String homeDir = System.getProperty("user.home");
+                                File novaPasta = new File(homeDir + File.separator + "MinhasMusicasSoundAura");
+                                if (!novaPasta.exists()) {
+                                    novaPasta.mkdir(); 
+                                }
+                                
                                 Main.alterarTelas("PaginaPrimaria");
                             }
                         }
